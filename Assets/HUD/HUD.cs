ee3636272c42115e1a53c3c761649c09d587d285
check324 <jiangtianyi888@gmail.com>
2016-03-28 12:56:54 -0700

    Tianyi Jiangtest2

--- Assets/HUD/HUD.cs
+++ Assets/HUD/HUD.cs
@@ -12,7 +12,7 @@ public class HUD : MonoBehaviour {
 
 	public Texture drone_cam_front, drone_cam_down;
 	
-	public Button cellBtn;
+	public Button cellBtn; //test
 
 	private const int RESOURCE_BAR_HEIGHT = 30;
 	private const int LINE_HEIGHT = 20;
